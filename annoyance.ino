c850f8c301877dc9b5f8a01c7938504bce7ff420
Sayre O'Bryan <obryans@bentonvillek12.org>
2023-03-26 20:26:42 -0500

    This one closes all tabs on chrome.

--- annoyance.ino
+++ annoyance.ino
@@ -7,7 +7,7 @@ void setup() {
 void loop() {
     DigiKeyboard.sendKeyStroke(0);
     Digikeyboard.delay(100);
-    DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT, KEY_W);
+    DigiKeyboard.sendKeyStroke(KEY_W, MOD_CONTROL_LEFT);
 }
 
 /*This one closes any tabs open on Chrome.*/
